f9f8ef5aa1722e2963bac316501df3b2723334b7
姬哀 <19585544+ji1ai1@users.noreply.github.com>
2021-08-10 17:55:12 +0800

    .

--- 預測.py
+++ 預測.py
@@ -5,7 +5,6 @@ import pandas
 import pickle
 import random
 import sklearn
-import sys
 
 print(str(datetime.datetime.now()) + "\t開始")
 
@@ -39,9 +38,13 @@ print(str(datetime.datetime.now()) + "\t開始")
 	+ ["用戸設備當日樣本數", "用戸作者當日樣本數", "用戸首人工標類當日樣本數", "用戸首機器標類當日樣本數"] + ["用戸類目%s當日樣本數" % 子 for 子 in range(5)] \
 
 
+訓練資料表 = pandas.concat([
+    pandas.DataFrame(numpy.concatenate([numpy.fromfile("資料/資料_標識%s" % 子, dtype=numpy.uint32) for 子 in range(1, 15)]).reshape([-1, 2]), columns=欄名[:2])
+    , pandas.DataFrame(numpy.concatenate([numpy.fromfile("資料/資料_%s" % 子, dtype=numpy.float16) for 子 in range(1, 15)]).reshape([-1, len(欄名) - 2]), columns=欄名[2:])
+], axis=1)
 測試資料表 = pandas.concat([
-    pandas.DataFrame(numpy.fromfile("data/資料_標識15", dtype=numpy.uint32).reshape([-1, 2]), columns=欄名[:2])
-    , pandas.DataFrame(numpy.fromfile("data/資料_15", dtype=numpy.float16).reshape([-1, len(欄名) - 2]), columns=欄名[2:])
+    pandas.DataFrame(numpy.fromfile("資料/資料_標識15", dtype=numpy.uint32).reshape([-1, 2]), columns=欄名[:2])
+    , pandas.DataFrame(numpy.fromfile("資料/資料_15", dtype=numpy.float16).reshape([-1, len(欄名) - 2]), columns=欄名[2:])
 ], axis=1)
 
 預測表 = 測試資料表.loc[:, ["用戸標識", "視訊標識", "設備"]]
@@ -57,15 +60,22 @@ print(str(datetime.datetime.now()) + "\t開始")
 }
 for 甲, 甲標籤型別 in enumerate(標籤型別):
 	選取特征 = [子 for 子, 子欄 in enumerate(測試資料表.columns) if 子 >= 3 + len(標籤型別) and not numpy.any([子欄.find(丑 + "標籤") > -1 for 丑 in 標籤型別 if 丑 not in 選擇標籤特征字典[甲標籤型別]])]
+	甲訓練資料表 = 訓練資料表.loc[訓練資料表.用戸標識.isin(訓練資料表[訓練資料表.iloc[:, 3 + 甲] == 1].用戸標識)]
+	print(str(datetime.datetime.now()) + "\t開始訓練第%s箇模型，選取%s箇特征, %s箇樣本" % (甲, len(選取特征), len(甲訓練資料表)))
 	
-	with open("data/輕模型%s" % 甲, "rb") as 档案:
-		輕模型[甲] = pickle.load(档案)
+	輕模型[甲] = lightgbm.train(
+		train_set=lightgbm.Dataset(甲訓練資料表.iloc[:, 選取特征], label=甲訓練資料表.iloc[:, 3 + 甲])
+		, num_boost_round=[4096, 1024, 4096, 2048, 2048, 1024, 2048][甲], params={"objective": "binary", "learning_rate": [0.05, 0.02, 0.05, 0.05, 0.02, 0.02, 0.02][甲], "lambda_l2": 10, "min_child_weight": 1.5, "max_depth": 6, "num_leaves": 32, "bagging_fraction": 0.7, "feature_fraction": 0.7, "num_threads": 64, "verbose": -1}
+	)
+	
+	with open("資料/輕模型%s" % 甲, "wb") as 档案:
+		pickle.dump(輕模型[甲], 档案)
 	
 	預測表[甲標籤型別 + "打分"] = 輕模型[甲].predict(測試資料表.iloc[:, 選取特征])
 
 
 提交表 = 預測表.loc[:, ["用戸標識", "視訊標識", "査評打分", "讚打分", "點撃頭像打分", "轉發打分", "收藏打分", "評論打分", "關注打分"]]
 提交表.columns = ["userid", "feedid", "read_comment", "like", "click_avatar", "forward", "favorite", "comment", "follow"]
-提交表.to_csv("data/ja.csv", index=False)
+提交表.to_csv("result.csv", index=False)
 
 print(str(datetime.datetime.now()) + "\t結束")
