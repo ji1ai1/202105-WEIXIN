9f8e34144773795cf0e8190a3ceb50101839f76f
姬哀 <19585544+ji1ai1@users.noreply.github.com>
2021-08-10 19:44:29 +0800

    .

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ cat 視訊.h | perl -pe 'BEGIN { binmode STDIN, ":utf8"; } s/(.)/ord($1) < 128 ?
 cat 行爲.h | perl -pe 'BEGIN { binmode STDIN, ":utf8"; } s/(.)/ord($1) < 128 ? $1 : sprintf("U%x", ord($1))/ge;' > U884cU7232.h
 cat 特征.h | perl -pe 'BEGIN { binmode STDIN, ":utf8"; } s/(.)/ord($1) < 128 ? $1 : sprintf("U%x", ord($1))/ge;' > U7279U5f81.h
 cat 微信特征提取器.cpp | perl -pe 'BEGIN { binmode STDIN, ":utf8"; } s/(.)/ord($1) < 128 ? $1 : sprintf("U%x", ord($1))/ge;' > 微信特征提取器U.cpp
-
+cat 微信測試特征提取器.cpp | perl -pe 'BEGIN { binmode STDIN, ":utf8"; } s/(.)/ord($1) < 128 ? $1 : sprintf("U%x", ord($1))/ge;' > 微信測試特征提取器U.cpp
 
 g++ -Wall -O3 -o 微信特征提取器 微信特征提取器U.cpp -lpthread
 g++ -Wall -O3 -o 微信測試特征提取器 微信測試特征提取器U.cpp -lpthread
